d9bf979d77e7ce2657fd36c0299f8d0cf7d0e787
Awoyo@20 <awoyooluwadamola@gmail.com>
2022-05-23 23:12:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+// function puntash(){
+//     let counter = 0
+//     if(counter <=6 return)
+// }
+
+
+
+
+
+// function multiply(a=0, b=0){
+//     console.log('$a * $b', a * b)
+//   a++
+//     b++
+//     if(b<=10) multiply(a,b);
+// }
+
+
+
+// for (i=10; i>0; i--){
+//     console.log(i)
+// }
+
+
+//  let i = 10
+//  while(i>=0){
+//      console.log(i)
+//      i--
+//  }
+
+
+
+//  let b = 10
+//  do{
+//      console.log(b)
+//      b--
+//  }while(b >=0)
+
+
+
+//  var total = 0
+//  for (b =0; b <=100; b++){
+//      total+=b
+//  }
+//  console.log(total)
+
+
+//  var total = 0
+//  for(b = 0; b <=100; b++){
+//      if (b % 2 ==0) total +=b
+//  }
+//  console.log(total)
+
+
+//  var oddtotal = 0
+//  for(b = 0; b <=100; b++){
+//      if(b % 2!=0) oddtotal +=b
+//  }
+// console.log(oddtotal)
+ 
+
+
+// var arr = []
+// arr.push(total)
+// arr.push(oddtotal)
+//     console.log(arr)
+
+
+
+
+
+
+
+
+
+var check = function(){
+    if (document.getElementById('password').value == 
+    
+    document.getElementById('checkPassword').value)
+{
+
+document.getElementById('alertPassword').style.color 
+= '#8cc63e'
+
+document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-check-circle"></i>Match ! </span>';
+} else{
+    document.getElementById('alertPassword').style.color ='#ee2b39';
+
+    document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-exclamation-triangle"></i>not matching</span>';
+}
+}
